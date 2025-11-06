1cc1dec14a20a33219b077c817586f9b9ba8de51
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2025-11-06 11:31:26 +0100

    Delete junk files (#26)

--- composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/App.kt
+++ composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/App.kt
@@ -1,10 +1,12 @@
 package com.swmansion.routinetracker
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
-import com.swmansion.routinetracker.screen.TestDatabaseScreen
+import androidx.compose.ui.Modifier
 
 @Composable
 fun App() {
-    MaterialTheme { TestDatabaseScreen() }
+    MaterialTheme { Column { Spacer(modifier = Modifier.weight(1f)) } }
 }
