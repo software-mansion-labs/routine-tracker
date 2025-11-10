710f858b30541652b5cc490061c06e8cee16f55a
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-11-10 12:49:53 +0100

    Styling

--- composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/screen/CreateRoutineScreen.kt
+++ composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/screen/CreateRoutineScreen.kt
@@ -1,6 +1,5 @@
 package com.swmansion.routinetracker.screen
 
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
