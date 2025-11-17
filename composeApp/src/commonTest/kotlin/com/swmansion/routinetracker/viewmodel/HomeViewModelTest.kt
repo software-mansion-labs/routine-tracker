bd797e16c750aac919901d9bde0a58fc6065fa07
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2025-11-17 12:37:21 +0100

    [#21] Add ViewModel tests (#35)
    * Add viewmodel tests
    
    * Change viewmodel tests
    
    * Fixes
    
    * Refactor

---
+++ composeApp/src/commonTest/kotlin/com/swmansion/routinetracker/viewmodel/HomeViewModelTest.kt
@@ -0,0 +1,121 @@
+package com.swmansion.routinetracker.viewmodel
+
+import com.swmansion.routinetracker.DataRepository
+import com.swmansion.routinetracker.DefaultDataRepository
+import com.swmansion.routinetracker.mock.database.MockRecurrenceDao
+import com.swmansion.routinetracker.mock.database.MockRoutineDao
+import com.swmansion.routinetracker.mock.database.MockTaskDao
+import com.swmansion.routinetracker.model.Routine
+import com.swmansion.routinetracker.utils.runTestWithMainDispatcher
+import kotlin.test.Test
+import kotlin.test.assertEquals
+import kotlin.test.assertTrue
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.advanceUntilIdle
+
+class HomeViewModelTest {
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun uiStateShouldInitiallyBeEmpty() = runTestWithMainDispatcher {
+        val repository = createMockRepository(flowOf(emptyList()))
+        val viewModel = HomeViewModel(repository)
+
+        assertTrue(viewModel.uiState.value.routinesWithTasks.isEmpty())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun uiStateShouldReflectRoutinesFromRepository() = runTestWithMainDispatcher {
+        val routines =
+            listOf(
+                Routine(id = 1L, name = "Morning Routine", time = "08:00"),
+                Routine(id = 2L, name = "Evening Routine", time = "20:00"),
+                Routine(id = 3L, name = "Workout Routine"),
+            )
+        val repository = createMockRepository(flowOf(routines))
+        val viewModel = HomeViewModel(repository)
+
+        advanceUntilIdle()
+
+        assertEquals(3, viewModel.uiState.value.routinesWithTasks.size)
+        assertEquals("Morning Routine", viewModel.uiState.value.routinesWithTasks[0].routine.name)
+        assertEquals("Evening Routine", viewModel.uiState.value.routinesWithTasks[1].routine.name)
+        assertEquals("Workout Routine", viewModel.uiState.value.routinesWithTasks[2].routine.name)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun uiStateShouldUpdateWhenRepositoryEmitsNewRoutines() = runTestWithMainDispatcher {
+        val stateFlow = MutableStateFlow<List<Routine>>(emptyList())
+        val repository = createMockRepository(stateFlow)
+        val viewModel = HomeViewModel(repository)
+
+        assertTrue(viewModel.uiState.value.routinesWithTasks.isEmpty())
+
+        val routines =
+            listOf(Routine(id = 1L, name = "Routine 1"), Routine(id = 2L, name = "Routine 2"))
+        stateFlow.value = routines
+
+        advanceUntilIdle()
+
+        assertEquals(2, viewModel.uiState.value.routinesWithTasks.size)
+        assertEquals("Routine 1", viewModel.uiState.value.routinesWithTasks[0].routine.name)
+        assertEquals("Routine 2", viewModel.uiState.value.routinesWithTasks[1].routine.name)
+
+        val updatedRoutines =
+            listOf(
+                Routine(id = 1L, name = "Routine 1"),
+                Routine(id = 2L, name = "Routine 2"),
+                Routine(id = 3L, name = "Routine 3"),
+            )
+        stateFlow.value = updatedRoutines
+
+        advanceUntilIdle()
+
+        assertEquals(3, viewModel.uiState.value.routinesWithTasks.size)
+        assertEquals("Routine 3", viewModel.uiState.value.routinesWithTasks[2].routine.name)
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun uiStateShouldHandleEmptyListFromRepository() = runTestWithMainDispatcher {
+        val repository = createMockRepository(flowOf(emptyList()))
+        val viewModel = HomeViewModel(repository)
+
+        advanceUntilIdle()
+
+        assertTrue(viewModel.uiState.value.routinesWithTasks.isEmpty())
+    }
+
+    @OptIn(ExperimentalCoroutinesApi::class)
+    @Test
+    fun uiStateShouldMaintainRoutineOrderFromRepository() = runTestWithMainDispatcher {
+        val routines =
+            listOf(
+                Routine(id = 1L, name = "A Routine"),
+                Routine(id = 2L, name = "B Routine"),
+                Routine(id = 3L, name = "C Routine"),
+            )
+        val repository = createMockRepository(flowOf(routines))
+        val viewModel = HomeViewModel(repository)
+
+        advanceUntilIdle()
+
+        assertEquals(3, viewModel.uiState.value.routinesWithTasks.size)
+
+        assertEquals("A Routine", viewModel.uiState.value.routinesWithTasks[0].routine.name)
+        assertEquals("B Routine", viewModel.uiState.value.routinesWithTasks[1].routine.name)
+        assertEquals("C Routine", viewModel.uiState.value.routinesWithTasks[2].routine.name)
+    }
+
+    private fun createMockRepository(routinesFlow: Flow<List<Routine>>): DataRepository {
+        val mockRoutineDao = MockRoutineDao(1L, routinesFlow)
+        val mockTaskDao = MockTaskDao()
+        val mockRecurrenceDao = MockRecurrenceDao()
+        return DefaultDataRepository(mockRoutineDao, mockTaskDao, mockRecurrenceDao)
+    }
+}
