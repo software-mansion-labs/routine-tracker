c0721782ef4aab7d9076ccbf099b06b4dd2eb0e3
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-11-18 11:25:33 +0100

    Remove KMPNotifier, add Alarmee

--- composeApp/src/androidMain/kotlin/com/swmansion/routinetracker/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/swmansion/routinetracker/MainActivity.kt
@@ -5,17 +5,13 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
-import com.mmk.kmpnotifier.permission.permissionUtil
 import com.swmansion.routinetracker.di.LocalAppContainer
 
 class MainActivity : ComponentActivity() {
-    private val permissionUtil by permissionUtil()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        permissionUtil.askNotificationPermission()
-
         val appContainer = (application as RoutineTrackerApplication).appContainer
 
         setContent { CompositionLocalProvider(LocalAppContainer provides appContainer) { App() } }
