ce231d866918536c025c9c053d9d77a57f6635fc
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-11-18 11:07:04 +0100

    Minor fixes

--- composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/data/UserPreferencesRepository.kt
+++ composeApp/src/commonMain/kotlin/com/swmansion/routinetracker/data/UserPreferencesRepository.kt
@@ -1,7 +1,6 @@
 package com.swmansion.routinetracker.data
 
 import com.swmansion.routinetracker.model.UserPreferences
-import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
 
 interface UserPreferencesRepository {
@@ -12,26 +11,4 @@ interface UserPreferencesRepository {
     suspend fun setSpecifiedTimeOption(option: String)
 
     suspend fun setUnspecifiedReminderTime(hour: Int, minute: Int)
-}
-
-fun createUserPreferencesRepository(): UserPreferencesRepository =
-    DefaultUserPreferencesRepository()
-
-class DefaultUserPreferencesRepository(initial: UserPreferences = UserPreferences()) :
-    UserPreferencesRepository {
-    private val mutable = MutableStateFlow(initial)
-    override val preferences: StateFlow<UserPreferences> = mutable
-
-    override suspend fun setRemindersEnabled(enabled: Boolean) {
-        mutable.value = mutable.value.copy(remindersEnabled = enabled)
-    }
-
-    override suspend fun setSpecifiedTimeOption(option: String) {
-        mutable.value = mutable.value.copy(specifiedTimeOption = option)
-    }
-
-    override suspend fun setUnspecifiedReminderTime(hour: Int, minute: Int) {
-        mutable.value =
-            mutable.value.copy(unspecifiedReminderHour = hour, unspecifiedReminderMinute = minute)
-    }
 }
