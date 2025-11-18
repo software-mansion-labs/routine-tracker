17d291a8cca6741f4eb4f3237e32a1986d6a03ef
Justyna Gręda <83274413+gredajustyna@users.noreply.github.com>
2025-11-18 08:15:32 +0100

    Basic notification setup and test (Android)

--- composeApp/src/androidMain/kotlin/com/swmansion/routinetracker/MainActivity.kt
+++ composeApp/src/androidMain/kotlin/com/swmansion/routinetracker/MainActivity.kt
@@ -5,12 +5,16 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.runtime.CompositionLocalProvider
+import com.mmk.kmpnotifier.permission.permissionUtil
 import com.swmansion.routinetracker.di.LocalAppContainer
 
 class MainActivity : ComponentActivity() {
+    private val permissionUtil by permissionUtil()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         enableEdgeToEdge()
         super.onCreate(savedInstanceState)
+        permissionUtil.askNotificationPermission()
 
         val appContainer = (application as RoutineTrackerApplication).appContainer
 
