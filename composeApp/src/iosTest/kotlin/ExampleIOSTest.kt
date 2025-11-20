a934384da47679db19d3c4bc4e337aa7af53f91c
Artur Gęsiarz <126520293+arturgesiarz@users.noreply.github.com>
2025-11-20 11:10:43 +0100

    [#22] Add iOS setup and Android setup for screenshot-based tests (#40)
    * Add ios setup
    
    * Add android setup
    
    * Delete useless gradle file
    
    * Refactor
    
    * Changes
    
    * Refactor

---
+++ composeApp/src/iosTest/kotlin/ExampleIOSTest.kt
@@ -0,0 +1,24 @@
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.ui.test.ExperimentalTestApi
+import androidx.compose.ui.test.onNodeWithText
+import androidx.compose.ui.test.onRoot
+import androidx.compose.ui.test.runComposeUiTest
+import com.github.takahirom.roborazzi.ExperimentalRoborazziApi
+import io.github.takahirom.roborazzi.captureRoboImage
+import kotlin.test.Test
+
+@OptIn(ExperimentalTestApi::class, ExperimentalRoborazziApi::class)
+class ExampleIOSTest {
+
+    @Test
+    fun test() {
+        runComposeUiTest {
+            setContent { MaterialTheme { Column { Button(onClick = {}) { Text("Hello World") } } } }
+            onRoot().captureRoboImage(this, filePath = "test_1.png")
+            onNodeWithText("Hello World").captureRoboImage(this, filePath = "test_2.png")
+        }
+    }
+}
